abb0536b41df6b74057f1ab46b24f4fe34352a6b
kojikawazu <nanairo7.daydream21@gmail.com>
2024-06-14 23:44:10 +0900

    エラーログ解析追加

--- src/app/api/[[...route]]/routeAuthCallback.ts
+++ src/app/api/[[...route]]/routeAuthCallback.ts
@@ -11,34 +11,51 @@ auth.get('/callback', async (c) => {
     const requestURL = new URL(c.req.url);
     const code = requestURL.searchParams.get("code");
 
+    console.log('Callback received:', { code });
+
     if (code) {
-        const supabase = supabaseRouteHandleClient();
-        const { data: { user } } = await supabase.auth.exchangeCodeForSession(code);
+        try {
+            const supabase = supabaseRouteHandleClient();
+            const { data, error } = await supabase.auth.exchangeCodeForSession(code);
 
-        if (user) {
-            const supabaseUserId = user.id;
+            if (error) {
+                console.error('Error exchanging code for session:', error);
+                return c.json({ message: 'Failed to exchange code for session' }, { status: 500 });
+            }
 
-            // データベースにユーザーが存在するか確認
-            const existingUser = await prisma.user.findFirst({
-                where: {
-                    id: supabaseUserId,
-                },
-            });
+            const user = data?.user;
+            console.log('User data:', user);
 
-            if (!existingUser) {
-                // ユーザーが存在しない場合はデータベースに追加
-                await prisma.user.create({
-                    data: {
+            if (user) {
+                const supabaseUserId = user.id;
+
+                // データベースにユーザーが存在するか確認
+                const existingUser = await prisma.user.findFirst({
+                    where: {
                         id: supabaseUserId,
-                        email: user.email ? user.email : "",
-                        fullName: user.user_metadata.full_name,
-                        avatarUrl: user.user_metadata.avatar_url,
                     },
                 });
-            }
-        }
 
-        return c.redirect(requestURL.origin);
+                console.log('Existing user:', existingUser);
+
+                if (!existingUser) {
+                    // ユーザーが存在しない場合はデータベースに追加
+                    await prisma.user.create({
+                        data: {
+                            id: supabaseUserId,
+                            email: user.email ? user.email : "",
+                            fullName: user.user_metadata.full_name,
+                            avatarUrl: user.user_metadata.avatar_url,
+                        },
+                    });
+                }
+            }
+
+            return c.redirect(requestURL.origin);
+        } catch (err) {
+            console.error('Unexpected error:', err);
+            return c.json({ message: 'Internal server error' }, { status: 500 });
+        }
     } else {
         return c.json({ message: 'Code not privider' }, { status: 400 });
     }
