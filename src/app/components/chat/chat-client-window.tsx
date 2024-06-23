31a7b11497317a9de09ccaf9d8490377c7449e06
kojikawazu <nanairo7.daydream21@gmail.com>
2024-06-23 18:22:39 +0900

    更新、削除の追加

--- src/app/components/chat/chat-client-window.tsx
+++ src/app/components/chat/chat-client-window.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { useChat } from '@/app/hooks/useChat';
 
@@ -25,15 +25,21 @@ const ChatClientWindow = ({
     if (userId === undefined) {
         router.push('/auth/signin');
     }
-    
+
+    const [isEditing, setIsEditing]     = useState<string | null>();
+    const [editMessage, setEditMessage] = useState<string>('');
+
     const {         
         messages,
+        setMessages,
         newMessage, 
         setNewMessage,
         connectionStatus,
         setConnectionStatus,
         fetchMessages,
         handleSendMessage,
+        handleUpdateMessage,
+        handleDeleteMessage,
         setReceivedMessage,
     } = useChat(userId);
 
@@ -54,7 +60,22 @@ const ChatClientWindow = ({
             console.log('[Front] WebSocket message received.');
             console.debug('data: ', event.data);
             const receivedData = JSON.parse(event.data);
-            setReceivedMessage(receivedData.messageWithUser);
+
+            if (receivedData.action === 'sendMessage') {
+                console.debug('[Front] sendMessage.');
+                setReceivedMessage(receivedData.message);
+            } else if (receivedData.action === 'updateMessage') {
+                console.debug('[Front] updateMessage.');
+                setMessages((prevMessages) =>
+                    prevMessages.map((message) =>
+                        message.id === receivedData.message.id ? receivedData.message : message
+                    )
+                );
+            } else if (receivedData.action === 'deleteMessage') {
+                console.debug('[Front] deleteMessage.');
+                setMessages((prevMessages) =>
+                    prevMessages.filter((message) => message.id !== receivedData.messageId));
+            } 
         };
 
         ws.onerror = (error: Event) => {
@@ -70,6 +91,17 @@ const ChatClientWindow = ({
         return () => ws.close();
     }, []);
 
+    const handleEditClick = (messageId: string, currentContent: string) => {
+        setIsEditing(messageId);
+        setEditMessage(currentContent);
+    }
+
+    const handleUpdateClick = async (messageId: string) => {
+        await handleUpdateMessage(messageId, editMessage);
+        setIsEditing(null);
+        setEditMessage('');
+    }
+
     return (
         <div className="flex flex-col w-full h-screen bg-gray-900 text-white">
             <div className="flex items-center justify-between h-16 border-b border-gray-600 px-4">
@@ -83,14 +115,62 @@ const ChatClientWindow = ({
                         <li 
                             key={message.id} 
                             className={`mb-2 flex flex-row ${message.user?.id === userId ? 'justify-end' : 'justify-start'}`}>
-                            <div className="flex flex-col">
+                            <div className="flex flex-col group">
                                 <div className="flex text-slate-300 text-sm">
                                     <strong className="ml-2">{message.user?.fullName || 'Unknown User'}</strong>
                                     <em>({new Date(message.createdAt).toLocaleString()})</em>
                                 </div>
-                                <div className={`px-6 py-3 rounded-t-full rounded-l-full max-w-xs lg:max-w-md ${message.user?.id === userId ? 'bg-blue-600' : 'bg-gray-700'}`}>
-                                    {message.content}
-                                    
+
+                                <div className={`flex ${message.user?.id === userId ? 'justify-end' : 'justify-start'}`}>
+                                    {message.user?.id === userId && (
+                                        <div className="flex space-x-s">
+                                            {isEditing === message.id.toString() ? (
+                                                <>
+                                                    <button 
+                                                        className="px-2 py-1 mr-2 bg-gray-500 rounded-full"
+                                                        onClick={() => handleUpdateClick(message.id.toString())}
+                                                    >
+                                                        Update
+                                                    </button>
+                                                    <button
+                                                        className="px-2 py-1 mr-2 bg-gray-500 rounded-full"
+                                                        onClick={() => setIsEditing(null)}
+                                                    >
+                                                        Cancel
+                                                    </button>
+                                                </>
+                                            ) : (
+                                                <>
+                                                    <button 
+                                                        className="px-2 py-1 mr-2 bg-gray-500 rounded-full hidden group-hover:block"
+                                                        onClick={() => handleEditClick(message.id.toString(), message.content)}
+                                                    >
+                                                        Edit
+                                                    </button>
+                                                    <button
+                                                        className="px-2 py-1 mr-2 bg-gray-500 rounded-full hidden group-hover:block"
+                                                        onClick={() => handleDeleteMessage(message.id.toString())}
+                                                    >
+                                                        Delete
+                                                    </button>
+                                                </>
+                                            )}
+                                        </div>
+                                    )}
+
+                                    <div className={`px-6 py-3 rounded-t-full rounded-l-full max-w-xs lg:max-w-md ${message.user?.id === userId ? 'bg-blue-600' : 'bg-gray-700'}`}>
+                                        {isEditing === message.id.toString() ? (
+                                            <input 
+                                                type="text"
+                                                placeholder="Please edit..."
+                                                value={editMessage}
+                                                className="px-2 py-1 rounded bg-blue-600 border"
+                                                onChange={(e) => setEditMessage(e.target.value)}
+                                            />                                    
+                                        ) : (
+                                            message.content
+                                        )}
+                                    </div>
                                 </div>
                             </div>
                         </li>
